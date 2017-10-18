1264cece70bcdb2ef15c3439a08370959c68ef88
shark <shark85423@gmail.com>
2017-10-18 10:34:10 +0800

    route初步建立

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * Route::get('/', function () {
+        return view('tasks');
+});
+
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+       //
+});
+
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+        //
+});
+
+ */
